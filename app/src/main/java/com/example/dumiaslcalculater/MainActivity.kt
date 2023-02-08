374afc0e8714103462624baa3a3b8f0d5e4590ae
Dumidu Fernando <90560618+DumiaSL@users.noreply.github.com>
2023-02-08 15:07:08 +0530

    Add Button

--- app/src/main/java/com/example/dumiaslcalculater/MainActivity.kt
+++ app/src/main/java/com/example/dumiaslcalculater/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.dumiaslcalculater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_clear : Button = findViewById(R.id.button_Clear)
+        button_Clear.setOnClickListener {
+
+            input.text = addToInputText("(")
+
+        }
+//        val btn_clear : Button = findViewById(R.id.button_Clear)
+//
+//        button_bracket_r.setOnClickListener {
+//
+//            input.text = addToInputText(")")
+//
+//        }
+
+
     }
 }
